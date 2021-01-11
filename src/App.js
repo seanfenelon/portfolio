6b889a87fd2ee3b7183cea0d94edc643e012298d
Sean Fenelon <sean.fenelon93@gmail.com>
2021-01-11 20:31:04 +0000

    okay

--- src/App.js
+++ src/App.js
@@ -10,17 +10,10 @@ import './styles/style.scss'
 
 
 const App = () => {
-  // ! Always start with the router at the top.
-  // ! Always use switch, for it to behave as you expect
-  // * SWITCH: Match the FIRST route inside the Switch.
-  // ? You'll give ROUTE 2 props
-  // ! 1) path (which is the path to render that route)
-  // ! 2) the actual component to show
-  return <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-    </Switch>
-  </BrowserRouter>
+
+  return <>
+  <Home />
+  </>
 }
 
 export default App
